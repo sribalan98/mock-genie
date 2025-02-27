ee1b3927d9a2bf6bc868570139daf3db4443112c
SriBalan <105964659+sribalan98@users.noreply.github.com>
2025-02-27 20:17:26 +0530

    Update Faker.ts

--- src/Faker/Faker.ts
+++ src/Faker/Faker.ts
@@ -3,13 +3,27 @@ import { faker } from '@faker-js/faker';
 // import { fakerDE as faker } from '@faker-js/faker';
 
 export const randomUser = (dataSize?: number): void => {
-  const name = faker.person.fullName({ sex: 'female' });
-  const email = faker.internet.email();
-  const phone = faker.phone.number();
+  const gender: 'male' | 'female' = 'male';
+  const firstName: string = faker.person.firstName(gender);
+  const lastName: string = faker.person.lastName(gender);
+  const fullName: string = faker.person.fullName({
+    firstName: firstName,
+    lastName: lastName,
+    sex: gender,
+  });
+
+  const email = faker.internet.email({
+    firstName: firstName,
+    lastName: lastName,
+    provider: 'google.com',
+  });
+  const phonePrefix: number = faker.helpers.arrayElement([6, 8, 7, 9]);
+  const phone = `+91-${phonePrefix}${faker.string.numeric(9)}`;
+
   const address = faker.location.streetAddress();
   const city = faker.location.city();
   const state = faker.location.state();
   const zip = faker.location.zipCode({ format: '######' });
   const country = faker.location.country();
-  console.log({ name, email, phone, address, city, state, zip, country });
+  console.log({ fullName, email, phone, address, city, state, zip, country });
 };
